d0415e92291c5fdc902bb7da904818ad4d8f8fc0
Sonia Paredes <55287768+soniaaparedees@users.noreply.github.com>
2026-06-04 11:12:13 -0400

    Create firebase.js config file

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC3-7fk-G7mSA9DSdfR217uVdDsWuk-2n4",
+  authDomain: "paddle-raise-tracker.firebaseapp.com",
+  databaseURL: "https://paddle-raise-tracker-default-rtdb.firebaseio.com",
+  projectId: "paddle-raise-tracker",
+  storageBucket: "paddle-raise-tracker.firebasestorage.app",
+  messagingSenderId: "238911011317",
+  appId: "1:238911011317:web:4f5b306447b5abb28dc8bc",
+  measurementId: "G-60N20XETTG"
+};
